be464402ed7b7fc91583dfae4bae9c1c08d8cd98
Opet321 <90963402+Opet321@users.noreply.github.com>
2026-01-30 19:59:54 +0700

    create downloader.py

---
+++ downloader.py
@@ -0,0 +1,11 @@
+import yt_dlp
+
+def download(query):
+    ydl_opts = {
+        "format": "best",
+        "outtmpl": "downloads/%(id)s.%(ext)s",
+        "quiet": True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(query, download=True)
+        return ydl.prepare_filename(info)
