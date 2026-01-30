3380785bfc7510f2b45b944d8292300ce36c2e45
Opet321 <90963402+Opet321@users.noreply.github.com>
2026-01-30 20:40:16 +0700

    update vc.py

--- vc.py
+++ vc.py
@@ -1,17 +1,17 @@
 from pytgcalls import PyTgCalls
-from pytgcalls.types import Update
-from pytgcalls.types import AudioPiped 
+from pytgcalls import types
+from pytgcalls.types import media 
 from assistant import assistant
 from helper_queue import get_next, has_queue 
 
 pytg = PyTgCalls(assistant)
 
 @pytg.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update):
+async def on_stream_end(client: PyTgCalls, update: types.Update):
     chat_id = update.chat_id
 
     if has_queue(chat_id):
         next_file = get_next(chat_id)
-        await pytg.play(chat_id, AudioPiped(next_file)) 
+        await pytg.play(chat_id, media.AudioPiped(next_file))
     else:
         await pytg.leave_call(chat_id)
